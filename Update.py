6f4c3b32e0e10b45176613d576d3fa8e4d2ac3aa
Hüseyin Serkan Özaydin <serkan.zaydn@gmail.com>
2017-05-28 07:57:23 +0300

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,6 +1,6 @@
 import BashCommands
 
 
-UpdateCommands=["sudo pacman -S git","git fetch"]
+UpdateCommands=["sudo pacman -S git","git pull"]
 def Update():
   BashCommands.RunCommands(UpdateCommands)
