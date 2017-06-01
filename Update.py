391f2ae8b7b9a6ba30037a79ccbf937ff0612bab
Hüseyin Serkan Özaydin <serkan.zaydn@gmail.com>
2017-06-01 06:55:56 +0300

    Update Update.py
    for workspace.xml

--- Update.py
+++ Update.py
@@ -3,6 +3,6 @@ import BashCommands
 #This class is being used to update the program
 #Bu class programın güncellenmesi için kullanıyor
 
-UpdateCommands=["sudo pacman -S git","git pull"]
+UpdateCommands=["git stash save --keep-index","sudo pacman -S git","git pull"]
 def Update():
   BashCommands.RunCommands(UpdateCommands)
