e7eda253e41eaa52f5649e0218007fd21eda3b28
hsmnzaydn <serkan.zaydn@gmail.com>
2017-06-03 07:03:10 +0300

    Changed Update.py

--- Update.py
+++ Update.py
@@ -3,6 +3,6 @@ import BashCommands
 #This class is being used to update the program
 #Bu class programın güncellenmesi için kullanıyor
 
-UpdateCommands=["sudo git stash save --keep-index","sudo pacman -S git","sudo git pull"]
+UpdateCommands=["sudo git stash save --keep-index --noconfirm","yaourt -S git --noconfirm","sudo git pull -noconfirm"]
 def Update():
   BashCommands.RunCommands(UpdateCommands)
